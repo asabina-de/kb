1d146448f7f09c4e2b75629afe356c19413c4226
David Asabina <vid@bina.me>
2025-07-14 17:47:02 +0200

    chore: define git-hook for markdown-link-check

--- devenv.nix
+++ devenv.nix
@@ -11,4 +11,13 @@
 
   # https://devenv.sh/git-hooks/
   git-hooks.hooks.prettier.enable = true;
+
+  # https://github.com/tcort/markdown-link-check/blob/master/.pre-commit-hooks.yaml
+  git-hooks.hooks.markdown-link-check = {
+    enable = true;
+    name = "Lint with markdown-lint-check";
+    entry = "markdown-link-check";
+    # https://pre-commit.com/#hooks-files
+    types = [ "markdown" ];
+  };
 }
