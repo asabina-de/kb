4a6efd1a905777d482238af32f2034dd22fe9c9d
David Asabina <david@asabina.de>
2025-09-10 22:14:09 +0200

    chore: drop nix-based pre-commit and install pre-commit utils

--- devenv.nix
+++ devenv.nix
@@ -11,20 +11,9 @@
 
   packages = with pkgs; [
     git
-    nodePackages.prettier
+    nixfmt-rfc-style
     nodePackages.markdown-link-check
+    nodePackages.prettier
+    pre-commit
   ];
-
-  # https://devenv.sh/git-hooks/
-  git-hooks.hooks.prettier.enable = true;
-  git-hooks.hooks.nixfmt-rfc-style.enable = true;
-
-  # https://github.com/tcort/markdown-link-check/blob/master/.pre-commit-hooks.yaml
-  git-hooks.hooks.markdown-link-check = {
-    enable = true;
-    name = "Lint with markdown-link-check";
-    entry = "markdown-link-check";
-    # https://pre-commit.com/#hooks-files
-    types = [ "markdown" ];
-  };
 }
