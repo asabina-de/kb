bf7d79420e1a9e4326a6a315e276bbc6a6c47faf
David Asabina <david@asabina.de>
2025-09-12 10:27:29 +0200

    chore: drop complexity idiomatic pre-commit setup takes over

--- devenv.nix
+++ devenv.nix
@@ -11,12 +11,7 @@
 
   packages = with pkgs; [
     git
-    # haskellPackages.cabal-install
-    # haskellPackages.ghc_9_12_1
-    husky
-    lint-staged
     nixfmt-rfc-style
-    nodePackages.markdown-link-check
     nodePackages.prettier
     pre-commit
     python3
